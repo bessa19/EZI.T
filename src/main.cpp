afb6d283abf25408a9606e295dfcfc3d5fac6a5b
bessa19 <bessajoao2003@gmail.com>
2025-03-02 21:31:26 +0000

    BLE Setup, needs further investigation

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BLEServer *pServer = NULL;
+BLEService *pService = NULL;
+BLECharacteristic *pCharacteristic = NULL;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("ESP32_GATE_CONTROL");
+  pServer = BLEDevice::createServer();
+  pService = pServer->createService(BLEUUID((uint16_t)0x180F)); // Generic Service
+
+  pCharacteristic = pService -> createCharacteristic(
+      BLEUUID((uint16_t)0x2A19), // Generic Characteristic
+      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+  );
+
+  pCharacteristic -> setValue("Hello from ESP32!");
+  pService -> start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->start();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    delay(2000);
 }
